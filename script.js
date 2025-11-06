b0e72054e6ecd601603e964966ce425957131212
Maria Alice <maria.a.costa30@aluno.senai.br>
2025-11-06 14:25:11 -0300

    teste

---
+++ script.js
@@ -0,0 +1,13 @@
+function Boasvindas() {
+        let Nome = prompt("digite seu Nome:");
+        alert("Bem-vindo,"+Nome+"! Vamos aprender JavaScript");
+
+        let Idade = prompt("digite sua Idade:");
+        alert("Bem-vindo,"+Idade+"! Vamos aprender JavaScript");
+
+        let CPF = prompt("digite seu CPF:");
+        alert("Bem-vindo,"+CPF+"! Vamos aprender JavaScript");
+
+        let EstadoCivil = prompt("digite seu Estado Civil:");
+        alert("Bem-vindo,"+EstadoCivil+"! Vamos aprender JavaScript");
+}
